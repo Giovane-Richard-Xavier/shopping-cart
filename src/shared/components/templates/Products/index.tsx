ad2626dffc9664de5d826de7c477c72c4da65929
Giovane-Richard-Xavier <giovane@rlvtecnologia.com.br>
2023-12-06 09:04:15 -0300

    fix/adjustes in Skeleton

--- src/shared/components/templates/Products/index.tsx
+++ src/shared/components/templates/Products/index.tsx
@@ -1,12 +1,12 @@
-import { useProducts } from '@/context/AppContext';
-import { ApiProducts } from '@/service/api/axios/products/apiProducts';
-import { useEffect, useState } from 'react';
-import { SkeletonCard } from '../../atom/Skeleton';
-import { ProductCard } from '../../organism/ProductCard';
-import styles from './styles.module.css';
+import { useProducts } from "@/context/AppContext";
+import { ApiProducts } from "@/service/api/axios/products/apiProducts";
+import React, { useEffect, useState } from "react";
+import { SkeletonCard } from "../../atom/Skeleton";
+import { ProductCard } from "../../organism/ProductCard";
+import styles from "./styles.module.css";
 
 export const Products = () => {
-  const {products, setProducts, loading, setLoading} = useProducts();
+  const { products, setProducts, loading, setLoading } = useProducts();
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
@@ -26,27 +26,28 @@ export const Products = () => {
   }, [offset]);
 
   // inifinity scroll
-  useEffect(() => {
-    const handleScroll = (e: any) => {
-      const scrollHeight = e.target.documentElement.scrollHeight;
-      const currentHeight = e.target.documentElement.scrollTop + window.innerHeight;
+  // useEffect(() => {
+  //   const handleScroll = (e: any) => {
+  //     const scrollHeight = e.target.documentElement.scrollHeight;
+  //     const currentHeight = e.target.documentElement.scrollTop + window.innerHeight;
 
-      if(currentHeight + 1 >= scrollHeight) {
-        setOffset(offset + 10);
-      }
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [offset]);
+  //     if(currentHeight + 1 >= scrollHeight) {
+  //       setOffset(offset + 10);
+  //     }
+  //   }
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, [offset]);
 
   return (
-    (loading && <SkeletonCard />)|| (
-    <section className={`${styles.products} container`}>
-      <div className={styles.products_item}>
-          
-        {products?.map((product) => <ProductCard key={product.id} data={product} />)}
-      </div>
-    </section>
+    (loading && <SkeletonCard />) || (
+      <section className={`${styles.products} container`}>
+        <div className={styles.products_item}>
+          {products?.map((product) => (
+            <ProductCard key={product.id} data={product} />
+          ))}
+        </div>
+      </section>
     )
-  )
-}
+  );
+};
